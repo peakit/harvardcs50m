fe3e43640b417f4ee83142e293ab615de3d49070
peakit <hmanak@gmail.com>
2022-11-20 16:54:27 +0530

    Implemented Add and Delete feature in TODO App

--- project0/script.js
+++ project0/script.js
@@ -10,5 +10,68 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  const li = document.createElement("li")
+  li.setAttribute("class", classNames.TODO_ITEM)
+  
+  // checkbox
+  const chkbox = document.createElement("input")
+  chkbox.setAttribute("type", "checkbox")
+  chkbox.setAttribute("class", classNames.TODO_CHECKBOX)
+  chkbox.setAttribute("onclick", "updateCount(this)")
+  
+  // TODO text
+  const todoText = document.createElement("input")
+  todoText.setAttribute("type", "texfield")
+  todoText.setAttribute("class", classNames.TODO_TEXT)
+  todoText.setAttribute("editable", true)
+
+  // Delete TODO button
+  const del = document.createElement("input")
+  del.setAttribute("type", "button")
+  del.setAttribute("class", classNames.TODO_DELETE)
+  del.setAttribute("value", "Delete!")
+  del.setAttribute("onclick", "delTODO(this)")
+
+  li.appendChild(chkbox)
+  li.appendChild(todoText)
+  li.appendChild(del)
+  list.appendChild(li)
+  incrementCounts()
+}
+
+function incrementCounts() {
+  totalCount = itemCountSpan.textContent
+  totalCount++
+  itemCountSpan.textContent = totalCount
+
+  uncheckedCount = uncheckedCountSpan.textContent
+  uncheckedCount++
+  uncheckedCountSpan.textContent = uncheckedCount
+}
+
+function updateCount(element) {
+  if (element.checked) {
+    // that means it was unchecked earlier, so
+    uncheckedCount--
+    uncheckedCountSpan.textContent = uncheckedCount
+  } else {
+    // that means it was checked earlier, so
+    uncheckedCount++
+    uncheckedCountSpan.textContent = uncheckedCount
+  }
+}
+
+function delTODO(element) {
+  decrementCounts(element)
+  element.parentElement.remove()
+}
+
+function decrementCounts(element) {
+  totalCount = itemCountSpan.textContent
+  totalCount--
+  itemCountSpan.textContent = Math.max(totalCount, 0)
+
+  uncheckedCount = uncheckedCountSpan.textContent
+  uncheckedCount--
+  uncheckedCountSpan.textContent = Math.max(uncheckedCount, 0)
 }
